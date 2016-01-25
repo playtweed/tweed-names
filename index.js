374a99434052704af560f0fde13cd8fddf51af4b
Tom McLean <tom.a.mclean@gmail.com>
2016-01-25 14:29:31 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var names = [
+    'Alice', 'Bob', 'Charlotte', 'Daniel', 'Eliza', 'Frank', 
+    'Gertrude', 'Harry', 'Ingrid', 'Jack', 'Lisa', 'Michael', 
+    'Nancy', 'Oscar', 'Patricia', 'Quentin', 'Rachel', 'Sam',
+    'Traci', 'Ulric', 'Vicky', 'Winston', 'Xanthe', 'Yanni', 'Zelda'
+];
+
+var idx = 0;
+
+function generate() {
+    var n = names[idx];
+    idx = (idx + 1) % names.length;
+}
+
+
+module.exports = generate;
